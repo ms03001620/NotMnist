bec66f8c2adcd7b7f9c02bee3aaaa187bc0b13bf
Mark <mazhenjin@sina.com>
2017-08-09 20:01:22 +0800

    LeNet-5 no dropout accuracy 95.7

--- train.py
+++ train.py
@@ -1,9 +1,14 @@
 import numpy as np
 import tensorflow as tf
 from six.moves import range
+from termcolor import colored
 
 from utils import data_util
 
+
+def get_weight(weight):
+    tf.add_to_collection("losses", tf.contrib.layers.l2_regularizer(0.001)(weight))
+    return weight
 
 def build_network(patch_size, image_size, num_channels, depth, num_labels, num_hidden):
     tf_train_dataset = tf.placeholder(tf.float32, shape=(None, image_size, image_size, num_channels), name='input')
@@ -16,38 +21,44 @@ def build_network(patch_size, image_size, num_channels, depth, num_labels, num_h
     layer2_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, depth, depth], stddev=0.1))
     layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
 
-    layer3_weights = tf.Variable(tf.truncated_normal([image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
+    layer3_weights = tf.Variable(tf.truncated_normal([num_hidden, num_hidden], stddev=0.1))
     layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
 
     layer4_weights = tf.Variable(tf.truncated_normal([num_hidden, num_labels], stddev=0.1))
+
     layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
 
-    def get_weight(weight):
-        tf.add_to_collection("losses", tf.contrib.layers.l2_regularizer(0.001)(weight))
-        return weight
+    def maxpool2d(x, k=2):
+        # MaxPool2D wrapper
+        return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],padding='SAME')
 
     # Model.
     def model(data):
-        conv = tf.nn.conv2d(data, layer1_weights, [1, 2, 2, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer1_biases)
+        strides = 2
+        conv = tf.nn.conv2d(data  , layer1_weights, strides = [1, strides, strides, 1], padding='SAME')
+        hidden = tf.nn.relu(tf.nn.bias_add(conv, layer1_biases))
+        hidden = maxpool2d(hidden, k=2)
 
-        conv = tf.nn.conv2d(hidden, layer2_weights, [1, 2, 2, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer2_biases)
+        conv = tf.nn.conv2d(hidden, layer2_weights, strides = [1, strides, strides, 1], padding='SAME')
+        hidden = tf.nn.relu(tf.nn.bias_add(conv, layer2_biases))
+        hidden = maxpool2d(hidden, k=2)
 
         shape = hidden.get_shape().as_list()
+        hidden = tf.reshape(hidden, [tf.shape(hidden)[0], shape[1] * shape[2] * shape[3]])
+        hidden = tf.add(tf.matmul(hidden, layer3_weights), layer3_biases)
 
-        reshape = tf.reshape(hidden, [tf.shape(hidden)[0], shape[1] * shape[2] * shape[3]])
+        hidden = tf.nn.relu(hidden)
 
-        hidden = tf.nn.relu(tf.nn.dropout((tf.matmul(reshape, layer3_weights) + layer3_biases), 0.5))
+        #hidden = tf.nn.dropout(hidden, 0.75)
 
-        return tf.nn.dropout((tf.matmul(hidden, layer4_weights) + layer4_biases), 0.5)
+        return tf.matmul(hidden, layer4_weights) + layer4_biases
 
     # Training computation.
     logits = model(tf_train_dataset)
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
 
     # Optimizer.
-    optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+    optimizer = tf.train.GradientDescentOptimizer(0.02).minimize(loss)
 
     # Predictions for the training, validation, and test data.
     train_prediction = tf.nn.softmax(logits, name="out_softmax")
@@ -62,10 +73,21 @@ def build_network(patch_size, image_size, num_channels, depth, num_labels, num_h
 
 
 def train_network(graph, batch_size, num_steps, train_dataset, train_labels, test_dataset, test_labels, pb_file_path):
+    threshold_train = 95.0
+    threshold_test = 96.0
+
     def accuracy(predictions, labels):
         return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
 
-    save_threshold = 80.0
+    def recording(accuracy, graph, pb_file_path, session, test_dataset, test_labels, threshold_test):
+        feed_test = {graph['x_placeholder']: test_dataset, graph['y_placeholder']: test_labels}
+        acc = accuracy(session.run(graph['train_prediction'], feed_dict=feed_test), test_labels)
+
+        print(colored('Recording Test accuracy: %.1f%%'% acc, 'red'))
+        if acc > threshold_test:
+            data_util.save_graph_to_pb(pb_file_path, session, "out_softmax")
+            return acc
+        return threshold_test
 
     with tf.Session() as session:
         tf.global_variables_initializer().run()
@@ -83,24 +105,22 @@ def train_network(graph, batch_size, num_steps, train_dataset, train_labels, tes
 
             if step % 100 == 0:
                 acc = accuracy(predictions, batch_labels)
-                print('%d\t step, accuracy %0.1f%% lost %f' % (step, acc, loss))
+                #print('%d\t step, accuracy %0.1f%% lost %f' % (step, acc, loss))
+                print('%d\t step, accuracy %0.1f%% ' % (step, acc))
 
-                if acc >= save_threshold:
-                    recording(accuracy, graph, pb_file_path, session, test_dataset, test_labels)
-                    save_threshold = acc
+                if acc >= threshold_train:
+                    threshold_test = recording(accuracy, graph, pb_file_path, session, test_dataset, test_labels, threshold_test)
+                    #threshold_train = acc
 
-        recording(accuracy, graph, pb_file_path, session, test_dataset, test_labels)
+        recording(accuracy, graph, pb_file_path, session, test_dataset, test_labels, threshold_test)
 
 
-def recording(accuracy, graph, pb_file_path, session, test_dataset, test_labels):
-    feed_test = {graph['x_placeholder']: test_dataset, graph['y_placeholder']: test_labels}
-    print('Recording Test accuracy: %.1f%%' % accuracy(session.run(graph['train_prediction'], feed_dict=feed_test), test_labels))
-    data_util.save_graph_to_pb(pb_file_path, session, "out_softmax")
+
 
 
 def main():
-    patch_size = 5
-    batch_size = 200
+    patch_size = 4
+    batch_size = 150
     image_size = 28
     num_channels = 1
     depth = 16
@@ -112,7 +132,7 @@ def main():
         = data_util.dataset_normalize(image_size, num_labels, pickle_path)
 
     graph = build_network(patch_size, image_size, num_channels, depth, num_labels, num_hidden)
-    num_steps = 1000
+    num_steps = 99999999
 
     pb_file_path = "output/not-mnist-a-j-tf1.2.pb"
 
